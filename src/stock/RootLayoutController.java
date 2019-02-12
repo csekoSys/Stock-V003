e1f8dbc98beac9a6bc67a0d9b737ce395538df85
Csenki Kornél <csenki.kornel@gmail.com>
2019-02-12 15:26:15 +0100

    test commit work 3 - 2019.02.12.

--- src/stock/RootLayoutController.java
+++ src/stock/RootLayoutController.java
@@ -1,18 +1,54 @@
 package stock;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 public class RootLayoutController implements Initializable {
 
-    
+    @FXML
+    private VBox contentVBox;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        contentVBox.getChildren().add(new Label("Kezdőképernyő"));
+    }
+
+    @FXML
+    private void addCashregisterTypeMenu(ActionEvent event) throws IOException {
+        try {
+            contentVBox.getChildren().add(loadWindow("/stock/cashregistertype/add/AddCashregisterType.fxml"));
+        } catch (IOException ex) {
+            System.err.println("Hiba a /stock/cashregistertype/add/AddCashregisterType.fxml betöltése közben");
+            Logger.getLogger(RootLayoutController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @FXML
+    private void addCashregisterTypeButton(ActionEvent event) {
+        try {
+            contentVBox.getChildren().add(loadWindow("/stock/cashregistertype/add/AddCashregisterType.fxml"));
+        } catch (IOException ex) {
+            System.err.println("Hiba a /stock/cashregistertype/add/AddCashregisterType.fxml betöltése közben");
+            Logger.getLogger(RootLayoutController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public Parent loadWindow(String url) throws IOException {
+        contentVBox.getChildren().clear();
+        Parent window = FXMLLoader.load(getClass().getResource(url));
+        return window;
+    }
+
 }
